b21271b3ee7d9cab8f88fd0e088b4aabe0735fa7
Vinicius Espindola <vinnyaoe@gmail.com>
2024-12-30 20:34:27 -0400

    ref: argocd namespaces

--- install-argocd.sh
+++ install-argocd.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-helm install argo-cd argo/argo-cd --version 7.7.11 --values argocd-helm/values.yml
+helm install argo-cd argo/argo-cd --version 7.7.11 --namespace argocd --create-namespace --values argocd-helm/values.yml
